e6089078182ff37ed983abf54ad3f3f3995b5d6b
Mohd Aadil <aaadil98138@gmail.com>
2025-03-09 12:29:02 +0530

    Initial commit

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setAuthUser } from "../redux/userSlice";
+import { BASE_URL } from "..";
+
+const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${BASE_URL}/api/v1/user/login`, user, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      navigate("/");
+      console.log(res);
+      dispatch(setAuthUser(res.data));
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
+  return (
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-3xl font-bold text-center">Login</h1>
+        <form onSubmit={onSubmitHandler} action="">
+          <div className="my-6">
+            {/* <label className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              className='w-full input input-bordered h-10'
+              type="text"
+              placeholder='Username' /> */}
+            <label className="input validator flex flex-row items-center gap-3">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </g>
+              </svg>
+              <input
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                type="input"
+                required
+                placeholder="Username"
+              />
+            </label>
+          </div>
+          <div>
+            {/* <label className='label p-2'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              className='w-full input input-bordered h-10'
+              type="password"
+              placeholder='Password' /> */}
+            <label className="input validator flex flex-row items-center gap-3">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                  <circle
+                    cx="16.5"
+                    cy="7.5"
+                    r=".5"
+                    fill="currentColor"
+                  ></circle>
+                </g>
+              </svg>
+              <input
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                type="password"
+                required
+                placeholder="Password"
+              />
+            </label>
+          </div>
+          <p className="text-center my-2">
+            Don't have an account? <Link to="/signup"> signup </Link>
+          </p>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
